abfd5dbdaa9baa9168f1f4ba60d515f2cbfedbe2
realTobby <tkattanek00@gmail.com>
2021-02-07 21:36:09 +0100

    Init
    - playfield can be created
    - field slots spawn at empty locations

---
+++ Zweitausendachtundvierzig/Zweitausendachtundvierzig/Game1.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Zweitausendachtundvierzig.Model;
+
+namespace Zweitausendachtundvierzig
+{
+    public class Game1 : Game
+    {
+        Random rnd = new Random();
+
+        Texture2D emptySlotTexture;
+        Texture2D slotTextureTwo;
+        Texture2D slotTextureFour;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        List<FieldSlot> playfieldCollection = new List<FieldSlot>();
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+            
+
+
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            RestartGame();
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            emptySlotTexture = Content.Load<Texture2D>("emptySlot");
+            slotTextureTwo = Content.Load<Texture2D>("slotTwo");
+            slotTextureFour = Content.Load<Texture2D>("slotFour");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            var kstate = Keyboard.GetState();
+
+            if(kstate.IsKeyDown(Keys.Space))
+            {
+                RestartGame();
+            }
+
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+            
+            foreach(FieldSlot playfieldSlot in playfieldCollection)
+            {
+                DrawFieldSlotAt(playfieldSlot.GetSlotValue(), playfieldSlot.GetPositionX(), playfieldSlot.GetPositionY());
+            }
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        public void RestartGame()
+        {
+            playfieldCollection.Clear();
+            playfieldCollection = new List<FieldSlot>();
+
+            for(int y = 0; y < 4; y++)
+            {
+                for(int x = 0; x < 4; x++)
+                {
+                    FieldSlot baseFieldSlot = new FieldSlot(x, y);
+                    playfieldCollection.Add(baseFieldSlot);
+                }
+            }
+
+            SpawnNewField();
+            SpawnNewField();
+            
+
+        }
+
+        public void SpawnNewField()
+        {
+            int randomFieldIndex = rnd.Next(playfieldCollection.Where(x=>x.GetFieldState() == FieldSlotOccupation.Empty).Count());
+
+            playfieldCollection[randomFieldIndex].Init();
+
+        }
+
+        private void DrawFieldSlotAt(int value, int indexX, int indexY)
+        {
+            Vector2 position = new Vector2(indexX * 102, indexY * 102);
+            switch (value)
+            {
+                case 0:
+                    _spriteBatch.Draw(emptySlotTexture, position, Color.White);
+                    break;
+                case 2:
+                    _spriteBatch.Draw(slotTextureTwo, position, Color.White);
+                    break;
+            }
+        }
+    }
+}
---
+++ Zweitausendachtundvierzig/Zweitausendachtundvierzig/Model/FieldSlot.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zweitausendachtundvierzig.Model
+{
+    public enum FieldSlotOccupation
+    {
+        Empty = 0,
+        Two = 2,
+        Four = 4,
+        Eight = 8,
+        Sixteen = 16,
+        ThirtyTwo = 32,
+        SixtyFour = 64,
+        OneHundredTwentyEight = 128,
+        TwoHundredFiftySix = 256,
+        FiveHundredTwelve = 512,
+        OneThousandTwentyFour = 1024,
+        TwoThousandFortyEight = 2048
+    }
+
+    public class FieldSlot
+    {
+        FieldSlotOccupation slotState;
+
+        private int fieldPositionX = 0;
+        private int fieldPositionY = 0;
+
+        private int slotValue = 0;
+
+        public int GetSlotValue()
+        {
+            return slotValue;
+        }
+
+        public int GetPositionX()
+        {
+            return fieldPositionX;
+        }
+
+        public int GetPositionY()
+        {
+            return fieldPositionY;
+        }
+
+
+        public FieldSlot(int x, int y)
+        {
+            slotState = FieldSlotOccupation.Empty;
+
+            fieldPositionX = x;
+            fieldPositionY = y;
+        }
+
+        public FieldSlotOccupation GetFieldState()
+        {
+            return slotState;
+        }
+
+        public void Init()
+        {
+            slotValue = 2;
+            slotState = FieldSlotOccupation.Two;
+        }
+
+    }
+}
